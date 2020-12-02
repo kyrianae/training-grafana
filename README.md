394ec65f5846cfea07f70171367bbec2c29e4efa
james <james.orlando@airbus.com>
2020-12-02 13:34:11 +0000

    merge appli and exporter

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # training-grafana
 docker-compose for training
+
+## Prerequisite
+Need docker and docker compose
---
+++ create_data.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p data/elasticsearch
+mkdir -p data/fluentd
+mkdir -p data/influxdb
+mkdir -p data/mariadb
+mkdir -p data/prometheus
